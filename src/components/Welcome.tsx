951d4b36593de80736d7031261f03af14f10c674
BrianMunene96 <107561877+BrianMunene96@users.noreply.github.com>
2023-03-04 20:49:27 +0100

    add welcome component

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -1,25 +1,34 @@
 import './Welcome.css'
 
 
-import { IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle, IonCardContent } from "@ionic/react";
+import { IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle, IonCardContent, IonButton } from "@ionic/react";
 import Button from './Button';
+import { useState } from 'react';
+import Dashboard from '../pages/Dashboard';
 
 const Welcome: React.FC = () => {
+  const [showDashboard, setShowDashboard] = useState(false)
+
+
+  if(showDashboard) {
+    return <Dashboard/>
+  } else {
     return (
-              
-            <IonCard>
-              <img alt="Silhouette of mountains" src="../../assets/icon/css.png" />
-              <IonCardHeader>
-                <IonCardTitle>CSS Module Quiz</IonCardTitle>
-                <IonCardSubtitle>Lets Learn CSS</IonCardSubtitle>
-              </IonCardHeader>
-                
-              <IonCardContent>
-                In this quiz, you'll test your understanding of CSS. This way you can find out what you still need to learn. Let's get Started!
-              </IonCardContent>
-              <Button/>
-            </IonCard>
-          );
+      <IonCard>
+        <img alt="Silhouette of mountains" src="../../assets/icon/css.png" />
+        <IonCardHeader>
+          <IonCardTitle>CSS Module Quiz</IonCardTitle>
+          <IonCardSubtitle>Lets Learn CSS</IonCardSubtitle>
+        </IonCardHeader>
+          
+        <IonCardContent>
+          In this quiz, you'll test your understanding of CSS. This way you can find out what you still need to learn. Let's get Started!
+        </IonCardContent>
+        <Button />
+      </IonCard>
+    );
+  }
+   
   };
   
   export default Welcome;
